81a2dd192a091aca00256c511bf4625165ac20cd
Darcisio Almeida <darcisioalmeida@gmail.com>
2022-09-27 13:08:07 -0300

    Criando Scroll no Menu e nas Sections da página
    Definindo scroll para click no Menu e posicionamento das Sections ao click.

--- nature-divinal/js/app.js
+++ nature-divinal/js/app.js
@@ -1,3 +1,4 @@
+// função para ativar os botoes //
 $('[data-group]').each(function(){
 	var $allTarget = $(this).find('[data-target]'),
 			$allClick = $(this).find('[data-click]'),
@@ -19,3 +20,59 @@ $('[data-group]').each(function(){
 		$(this).addClass(activeClass);
 	});
 });
+
+// função para scroll ao click no menu //
+$('.menu-nav a[href^="#"]').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('.menu').innerHeight(),
+			targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
+
+// função para click scroll ao click na logo
+$('.logo').click(function(e){
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: 0
+	}, 500)
+});
+
+// função para scroll na em cada section //
+$('section').each(function(){
+	var height = $(this).height(),
+			offsetTop = $(this).offset().top,
+			menuHeight = $('.menu').innerHeight(),
+			id = $(this).attr('id'),
+			$itemMenu = $('a[href="#' + id + '"]');
+	
+	$(window).scroll(function(){
+		var scrollTop = $(window).scrollTop();
+		if(offsetTop - menuHeight < scrollTop && offsetTop + height - menuHeight > scrollTop) {
+			$itemMenu.addClass('active');
+		} else {
+			$itemMenu.removeClass('active');
+		}
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
